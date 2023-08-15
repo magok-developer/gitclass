814ba71171c321cf0d73ab2d20ae5ca2201f2cfa
jiwon <hajw.study@gmail.com>
2023-08-15 14:02:52 +0900

    15일 마지막 수업

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+console.log('hallo javascript');
+
+var age = 25;
+
+let year = 2023;
+
+const point = 250;
+
+console.log(age);
+console.log(year);
+console.log(point);
+
+const pass = true;
+
+if (pass) {
+  console.log ('hello')
+}
